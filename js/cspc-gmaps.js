e297477a17bb58f73ae45a65927c15e5307e9e59
Prabh <prabhg@yahoo-inc.com>
2014-05-11 20:05:04 -0700

    removed click handler for maps onclick

--- js/cspc-gmaps.js
+++ js/cspc-gmaps.js
@@ -14,27 +14,6 @@ function initializeMapApi() {
         zoom: 8
     };
     CSPC.map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-
-    setupGoogleMapEventListeners(CSPC.map);
-    //addFiveRandomMarkers(CSPC.map);
-
-    //CSPC.map.getCenter()
-    //CSPC.map.setCenter(location);
-}
-
-// google maps event listeners
-function setupGoogleMapEventListeners(map){
-    google.maps.event.addListener(map, 'center_changed', function() {
-        // 3 seconds after the center of the map has changed, pan back to the
-        // marker.
-        window.setTimeout(function() {
-            map.panTo(marker.getPosition());
-        }, 3000);
-    });
-
-    google.maps.event.addListener(map, 'click', function(event) {
-        placeMarker(map, event.latLng);
-    });
 }
 
 function placeMarker(map, location) {
@@ -47,8 +26,6 @@ function placeMarker(map, location) {
 function geoCodeAddress(address, callback) {
     CSPC.geocoder.geocode( { 'address': address}, function(results, status) {
         if (status == google.maps.GeocoderStatus.OK) {
-            //map.setCenter(results[0].geometry.location);
-
             callback(results[0].geometry.location);
         } else {
             console.log("Geocode was not successful for the following reason: " + status);
@@ -75,34 +52,3 @@ function addMarkersAtAddresses(addrArr){
         geoCodeAddress(addrArr[i], getAddrLatLang);
     }
 }
-
-function addFiveRandomMarkers(map) {
-    // Add 5 markers to the map at random locations.
-    var southWest = new google.maps.LatLng(-31.203405,125.244141);
-    var northEast = new google.maps.LatLng(-25.363882,131.044922);
-    var bounds = new google.maps.LatLngBounds(southWest,northEast);
-    map.fitBounds(bounds);
-    var lngSpan = northEast.lng() - southWest.lng();
-    var latSpan = northEast.lat() - southWest.lat();
-
-    for (var i = 0; i < 5; i++) {
-        var location = new google.maps.LatLng(southWest.lat() + latSpan * Math.random(),
-                southWest.lng() + lngSpan * Math.random());
-        var marker = placeMarker(map, location);
-        marker.setTitle("Marker #" + (i+1));
-
-        var infowindow = new google.maps.InfoWindow(
-            { content: "This is marker #" + (i+1),
-                size: new google.maps.Size(100,150)
-            }
-        );
-        marker.infoWindow = infowindow;
-        google.maps.event.addListener(marker, 'click', function() {
-            var thisMarker = this;
-            thisMarker.infoWindow.open(map, thisMarker);
-            map.setZoom(8);
-            map.setCenter(thisMarker.getPosition());
-        });
-    }
-}
-
